6817d03f4def6612ecaff825b6de03182676f939
prashantkumar1113 <prashantkumar1113@gmail.com>
2021-10-30 13:22:23 -0400

    Completed most functionality, need to draw the bubbles

--- index.js
+++ index.js
@@ -7,18 +7,45 @@ const images = [
     './assets/6.jpg'
 ]
 
-let currentImg = 1;
-let slideShoeImage = document.querySelector('.image')
+let currentImg = 0;
+const slideShoeImage = document.querySelector('.image')
 slideShoeImage.setAttribute('src', images[currentImg])
 
-let prevBtn = document.querySelector('.previous side-bar')
+const prevBtn = document.querySelector('.previous')
 prevBtn.addEventListener('click', function(e){
-    //to do
+    //check if your zero
+    //console.log('prev')
+    if(currentImg === 0){
+        currentImg = images.length-1
+    }
+    else{
+        currentImg--
+    }
+    slideShoeImage.setAttribute('src', images[currentImg])
 })
 
-let nextBtn = document.querySelector('.next side-bar')
+const nextBtn = document.querySelector('.next')
+console.log(nextBtn)
 nextBtn.addEventListener('click', function(e){
-    //to do
+    //check if your at the max
+    //console.log('next')
+    if(currentImg === images.length-1){
+        currentImg = 0
+    }
+    else{
+        currentImg++
+    }
+    slideShoeImage.setAttribute('src', images[currentImg])
 })
 
-let bubbles = document.querySelectorAll('.bubble')
+const bubbles = document.querySelectorAll('.bubble')
+for (let i = 0; i < bubbles.length; i++){
+    bubbles[i].addEventListener('click', function(e){
+        //console.log('bubble')
+        //console.log(parseFloat(bubbles[i].id.slice(-1)))
+        currentImg = parseFloat(bubbles[i].id.slice(-1))
+        slideShoeImage.setAttribute('src', images[currentImg])
+    })
+}
+
+//○● TODO: need to draw the bubbles
