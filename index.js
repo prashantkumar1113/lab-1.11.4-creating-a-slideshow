e3e63124c2674f1d8faebcf20cfa36dbaf6cd598
prashantkumar1113 <prashantkumar1113@gmail.com>
2021-10-30 00:29:51 -0400

    Started coded the slideshow

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ const images = [
     './assets/5.jpg',
     './assets/6.jpg'
 ]
+
+let currentImg = 1;
+let slideShoeImage = document.querySelector('.image')
+slideShoeImage.setAttribute('src', images[currentImg])
+
+let prevBtn = document.querySelector('.previous side-bar')
+prevBtn.addEventListener('click', function(e){
+    //to do
+})
+
+let nextBtn = document.querySelector('.next side-bar')
+nextBtn.addEventListener('click', function(e){
+    //to do
+})
+
+let bubbles = document.querySelectorAll('.bubble')
